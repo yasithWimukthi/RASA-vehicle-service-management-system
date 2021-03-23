bb76c29673cc6db16fffadddea2bf1fb8fabc350
yasithWimukthi <it19966922@my.sliit.lk>
2021-03-23 20:18:16 +0530

    create InsuranceService model class

---
+++ src/com/rasa/model/InsuranceService.java
@@ -0,0 +1,59 @@
+package com.rasa.model;
+
+public class InsuranceService extends Service{
+    private boolean customerNoObjection;
+    private boolean leasingNoObjection;
+    private boolean claimForm;
+
+    public InsuranceService(
+            String NICno,
+            String vehicleRegistrationNo,
+            String entryDate,
+            String accidentDate,
+            String paymentType,
+            boolean customerNoObjection,
+            boolean leasingNoObjection,
+            boolean claimForm) {
+        super(NICno, vehicleRegistrationNo, entryDate, accidentDate, paymentType);
+        this.customerNoObjection = customerNoObjection;
+        this.leasingNoObjection = leasingNoObjection;
+        this.claimForm = claimForm;
+    }
+
+    public InsuranceService(
+            String entryDate,
+            String accidentDate,
+            String paymentType,
+            boolean customerNoObjection,
+            boolean leasingNoObjection,
+            boolean claimForm) {
+        super(entryDate, accidentDate, paymentType);
+        this.customerNoObjection = customerNoObjection;
+        this.leasingNoObjection = leasingNoObjection;
+        this.claimForm = claimForm;
+    }
+
+    public boolean isCustomerNoObjection() {
+        return customerNoObjection;
+    }
+
+    public void setCustomerNoObjection(boolean customerNoObjection) {
+        this.customerNoObjection = customerNoObjection;
+    }
+
+    public boolean isLeasingNoObjection() {
+        return leasingNoObjection;
+    }
+
+    public void setLeasingNoObjection(boolean leasingNoObjection) {
+        this.leasingNoObjection = leasingNoObjection;
+    }
+
+    public boolean isClaimForm() {
+        return claimForm;
+    }
+
+    public void setClaimForm(boolean claimForm) {
+        this.claimForm = claimForm;
+    }
+}
