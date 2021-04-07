7f02ae28cee488dfe6cf10e7e6ba5e8b7e3fe6f3
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-07 22:30:17 +0530

    Implement add customer method

--- src/com/rasa/util/CustomerManagementQuery.java
+++ src/com/rasa/util/CustomerManagementQuery.java
@@ -2,4 +2,5 @@ package com.rasa.util;
 
 public class CustomerManagementQuery {
     public static final String SEARCH_CLIENT_BY_NIC = "SELECT * FROM client WHERE NICnumber = ?";
+    public static final String ADD_CUSTOMER = "INSERT INTO client (NICnumber,firstName,lastName,email,address,mobile,OperatorID) VALUES (?,?,?,?,?,?,?) ";
 }
