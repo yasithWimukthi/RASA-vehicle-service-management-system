197bacd91cac6eef66c79729c4d7ee9d3874bea6
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-07 22:42:53 +0530

    Implement add customer servlet

--- src/com/rasa/servlet/AddCustomerServlet.java
+++ src/com/rasa/servlet/AddCustomerServlet.java
@@ -7,6 +7,9 @@ package com.rasa.servlet;
  * IT19966922
  **/
 
+import com.rasa.service.ClientService;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +19,15 @@ import java.io.IOException;
 
 @WebServlet(name = "AddCustomerServlet")
 public class AddCustomerServlet extends HttpServlet {
+
+    ClientService clientService;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        clientService = new ClientService();
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String fname = request.getParameter("fname");
         String lname = request.getParameter("lname");
@@ -23,6 +35,17 @@ public class AddCustomerServlet extends HttpServlet {
         String mobile = request.getParameter("mobile");
         String nic = request.getParameter("nic");
         String address = request.getParameter("address");
+        String operatorID = request.getParameter("operatorID");
+
+        boolean isSuccess = clientService.addCustomer(nic,fname,lname,mobile,address,email,operatorID);
+
+        RequestDispatcher dispatcher;
+        if(isSuccess){
+            dispatcher = getServletContext().getRequestDispatcher("/vehiclereg.jsp");
+        }else {
+            dispatcher = getServletContext().getRequestDispatcher("/customerreg.jsp");
+        }
+        dispatcher.forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
