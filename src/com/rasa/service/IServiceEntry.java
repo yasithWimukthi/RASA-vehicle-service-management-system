049c443dda4823acbdc8837ab91a71b6a8a58569
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-09 21:45:12 +0530

    Implement service entry service class

--- src/com/rasa/service/IServiceEntry.java
+++ src/com/rasa/service/IServiceEntry.java
@@ -7,6 +7,27 @@ package com.rasa.service;
  * IT19966922
  **/
 
-public class IServiceEntry {
+public interface IServiceEntry {
 
+    /**
+     * addServiceEntry
+     * add service details into the service table
+     * @param registrationNumber
+     * @param ServiceType
+     * @param entryDate
+     * @param accidentDate
+     * @param customerNoObjection
+     * @param insuranceNoObjection
+     * @param claimForm
+     * @return return true if insert successfully, otherwise false
+     */
+    boolean addServiceEntry(
+            String registrationNumber,
+            String ServiceType,
+            String entryDate,
+            String accidentDate,
+            boolean  customerNoObjection,
+            boolean insuranceNoObjection,
+            boolean claimForm
+    );
 }
--- src/com/rasa/util/CustomerManagementQuery.java
+++ src/com/rasa/util/CustomerManagementQuery.java
@@ -6,4 +6,6 @@ public class CustomerManagementQuery {
     public static final String SEARCH_VEHICLE_BY_REGISTRATION_NUMBER = "SELECT * FROM vehicle WHERE registrationNumber = ? " ;
     public static final String ADD_VEHICLE = "INSERT INTO vehicle (registrationNumber,manufacturedYear,brand,model,color) VALUES (?,?,?,?,?) " ;
     public static final String ADD_OWNER = "INSERT INTO veicleOwner (NICnumber,registrationNumber) VALUES (?,?) " ;
+    public static final String ADD_CASH_SERVICE = "INSERT INTO service (registrationNumber,entryDate,accidentDate) VALUES (?,?,?)" ;
+    public static final String ADD_INSURANCE_SERVICE = "INSERT INTO service (registrationNumber,entryDate,accidentDate,customerNoObjection,insuranceNoObjection,claimForm) VALUES(?,?,?,?,?,?)" ;
 }
