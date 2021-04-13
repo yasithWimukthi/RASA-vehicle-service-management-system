a0ee7edee52f5d16c4d99ca07b4a13153ca8bf94
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-13 22:45:07 +0530

    modify service entry registration

--- src/com/rasa/util/CustomerManagementQuery.java
+++ src/com/rasa/util/CustomerManagementQuery.java
@@ -6,10 +6,12 @@ public class CustomerManagementQuery {
     public static final String SEARCH_VEHICLE_BY_REGISTRATION_NUMBER = "SELECT * FROM vehicle WHERE registrationNumber = ? " ;
     public static final String ADD_VEHICLE = "INSERT INTO vehicle (registrationNumber,manufacturedYear,brand,model,color) VALUES (?,?,?,?,?) " ;
     public static final String ADD_OWNER = "INSERT INTO veicleOwner (NICnumber,registrationNumber) VALUES (?,?) " ;
-    public static final String ADD_CASH_SERVICE = "INSERT INTO service (registrationNumber,entryDate,accidentDate) VALUES (?,?,?)" ;
-    public static final String ADD_INSURANCE_SERVICE = "INSERT INTO service (registrationNumber,entryDate,accidentDate,customerNoObjection,insuranceNoObjection,claimForm) VALUES(?,?,?,?,?,?)" ;
+    public static final String ADD_CASH_SERVICE = "INSERT INTO service (registrationNumber,entryDate,accidentDate,type) VALUES (?,?,?,?)" ;
+    public static final String ADD_INSURANCE_SERVICE = "INSERT INTO service (registrationNumber,entryDate,accidentDate,customerNoObjection,insuranceNoObjection,claimForm,type) VALUES(?,?,?,?,?,?,?)" ;
     public static final String SEARCH_ENTRY_BY_REG_NUM = "SELECT * FROM service where registrationNumber = ? " ;
     public static final String UPDATE_CLIENT = "UPDATE client SET firstName = ?, lastName = ?, email = ?, address = ?, mobile = ? WHERE NICnumber = ? " ;
     public static final String UPDATE_VEHICLE = "UPDATE vehicle SET manufacturedYear = ?, brand = ?, model = ?, color = ? WHERE registrationNumber = ?" ;
     public static final String SEARCH_ENTRY_BY_NIC_NUM = "SELECT * FROM service,veicleOwner WHERE service.registrationNumber = veicleOwner.registrationNumber AND veicleOwner.NICnumber = ? " ;
+    public static final String ADD_INSURANCE_SERVICE_ENTRY = "INSERT INTO service (registrationNumber,entryDate,accidentDate,customerNoObjection,insuranceNoObjection,claimForm) VALUES (?,?,?,?,?,?)" ;
+    public static final String ADD_NORMAL_SERVICE_ENTRY = "INSERT INTO service (registrationNumber,entryDate,accidentDate) VALUES (?,?,?)" ;
 }
