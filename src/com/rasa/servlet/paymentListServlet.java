96714b98c3a2553c8bf0f753c7797f6f758cb11c
tharukshi724 <tharukshiw@gmail.com>
2021-04-14 15:21:39 +0530

    modify paymentListServlet file

--- src/com/rasa/servlet/paymentListServlet.java
+++ src/com/rasa/servlet/paymentListServlet.java
@@ -1,50 +1,47 @@
 package com.rasa.servlet;
 
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.util.List;
 
-@WebServlet(name = "paymentListServlet")
+import com.rasa.model.paymentList;
+import com.rasa.service.paymentdao;
+
+@WebServlet("/paymentListServlet")
 public class paymentListServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       this.doGet(request, response);
+
+
+        String vehicleRegiNo = request.getParameter("vehicleRegiNo");
+        String customerName = request.getParameter("customerName");
+        double estimateAmount = Double.parseDouble(request.getParameter("estimateAmount"));
+
+        double cash = Double.parseDouble(request.getParameter("cash"));
+        Date paymentDate = Date.valueOf(request.getParameter("paymentDate"));
+
+
+        paymentList newPayment = new paymentList(vehicleRegiNo, customerName, estimateAmount, cash, paymentDate);
+        try {
+            paymentdao.addPayment(newPayment);
+            response.sendRedirect("paymentlist.jsp");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
     }
+
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String action = request.getServletPath();
 
-        switch(action){
-            case "/insert":
-                   addPayment(request, response);
-                break;
-            case "/delete":
-                   deletePayment(request,response);
-                   break;
-            case "/update":
-                   updatePayment(request,response);
-                   break;
-            default:
-                paymentList(request,response);
-                break;
-
-
-        }
-    }
-
-    private void paymentList(HttpServletRequest request, HttpServletResponse response) {
-    }
-
-    private void updatePayment(HttpServletRequest request, HttpServletResponse response) {
-    }
-
-    private void deletePayment(HttpServletRequest request, HttpServletResponse response) {
-    }
-
-    private void addPayment(HttpServletRequest request, HttpServletResponse response) {
     }
 }
+
