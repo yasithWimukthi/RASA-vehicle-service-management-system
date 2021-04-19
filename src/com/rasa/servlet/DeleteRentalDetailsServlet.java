2f5e17bd3f4ed54c29b32e0aa2e833d773dac90d
ASUS <nethminisansala99@gmail.com>
2021-04-20 04:07:03 +0530

    Delete servlet is created

--- src/com/rasa/servlet/DeleteRentalDetailsServlet.java
+++ src/com/rasa/servlet/DeleteRentalDetailsServlet.java
@@ -1,15 +1,33 @@
 package com.rasa.servlet;
 
+import com.rasa.service.IRentCarService;
+import com.rasa.service.RentCarService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/DeleteRentalDetailsServlet")
 public class DeleteRentalDetailsServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int rentID = Integer.parseInt(request.getParameter("rid"));
+
+        IRentCarService iRentCarService = new RentCarService();
+        try {
+            boolean isDeleted = iRentCarService.deleteRentalDetails(rentID);
+            if(!isDeleted){
+                request.getRequestDispatcher("rentalDetails.jsp").forward(request, response);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 
