ea1b969d39f29456f87f23d638d288ffb948773c
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-13 22:30:50 +0530

    Edit update client

--- src/com/rasa/servlet/UpdateClientServlet.java
+++ src/com/rasa/servlet/UpdateClientServlet.java
@@ -8,7 +8,12 @@ package com.rasa.servlet;
  **/
 
 
+import com.rasa.model.Customer;
+import com.rasa.model.Repair;
+import com.rasa.model.Vehicle;
 import com.rasa.service.ClientService;
+import com.rasa.service.ServiceEntry;
+import com.rasa.service.VehicleService;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -21,12 +26,23 @@ import java.io.IOException;
 @WebServlet("/UpdateClientServlet")
 public class UpdateClientServlet extends HttpServlet {
 
-    ClientService clientService;
+    private ClientService clientService;
+    private VehicleService vehicleService;
+    private ServiceEntry serviceEntry;
+    private Repair repair;
+    private Vehicle vehicle;
+    private Customer customer;
 
     @Override
     public void init() throws ServletException {
         super.init();
         clientService = new ClientService();
+        vehicleService = new VehicleService();
+        serviceEntry = new ServiceEntry();
+        clientService = new ClientService();
+        repair = new Repair();
+        vehicle = new Vehicle();
+        customer = new Customer();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -37,14 +53,22 @@ public class UpdateClientServlet extends HttpServlet {
         String mobile = request.getParameter("mobile");
         String nic = request.getParameter("nic");
         String address = request.getParameter("address");
+        int serviceId = Integer.parseInt(request.getParameter("repairId"));
+        String regID = request.getParameter("registrationNumber");
         boolean isSuccess = false;
 
         isSuccess = clientService.updateCustomer(nic,fname,lname,mobile,address,email);
 
-        RequestDispatcher dispatcher;
-        dispatcher = getServletContext().getRequestDispatcher("/searchservice.jsp");
-        dispatcher.forward(request, response);
+        repair = serviceEntry.getRepairByServiceID(serviceId);
+        customer = clientService.searchByNic(repair.getNICno().toUpperCase());
+        vehicle = vehicleService.searchByRegistrationNumber(repair.getVehicleRegistrationNo().toUpperCase());
 
+        request.setAttribute("repair",repair);
+        request.setAttribute("client",customer);
+        request.setAttribute("vehicle",vehicle);
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/entry.jsp");
+        dispatcher.forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
