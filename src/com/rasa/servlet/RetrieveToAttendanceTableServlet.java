f9b0b82c2d65e6794090b3fc36413fc4ea50eb87
iamKAviB <iamkavib@gmail.com>
2021-05-16 14:05:06 +0530

    retrieve to attendance

--- src/com/rasa/servlet/RetrieveToAttendanceTableServlet.java
+++ src/com/rasa/servlet/RetrieveToAttendanceTableServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "RetrieveToAttendanceTableServlet")
+@WebServlet("/RetrieveToAttendanceTableServlet")
 public class RetrieveToAttendanceTableServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
