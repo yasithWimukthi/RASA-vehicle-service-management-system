a5f95138e00e845044cd69b5a4c8243d5db6b9ad
tharukshi724 <tharukshiw@gmail.com>
2021-05-17 19:14:56 +0530

    modify addPaymentservice

--- src/com/rasa/service/addPaymentService.java
+++ src/com/rasa/service/addPaymentService.java
@@ -1,8 +1,8 @@
 package com.rasa.service;
 
+
 import com.rasa.model.addpayment;
 import com.rasa.util.DBConnectionUtil;
-
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -12,14 +12,16 @@ import java.sql.SQLException;
 public class addPaymentService {
     static Connection con;
 
-    public double CalcTotalEstimates(String type , int serviceID) throws SQLException, ClassNotFoundException {
+    public double CalcTotalEstimates(String type , int sid) throws SQLException, ClassNotFoundException {
         con = DBConnectionUtil.getConnection();
         double Totalestimate = 0.0;
         double Eamount = 0.0;
 
-        String query = "SELECT vi.estimateAmount FROM  rasa.workprogress w , rasa.repair_service rs , rasa.vehiclerepair_item vi WHERE  w.pid = rs.pid AND rs.ser_id = vi.ser_id AND w.serviceID = ? AND rs.ser_type =?";
+        totEstimates(sid);
+
+        String query = "SELECT vi.estimateAmount FROM  rasa.workprogress w , rasa.repair_service rs , rasa.vehiclerepair_item vi WHERE  w.pid = rs.pid AND rs.ser_id = vi.ser_id AND w.sid= ? AND rs.ser_type =?";
         PreparedStatement preparedStatement = con.prepareStatement(query);
-        preparedStatement.setInt(1,serviceID);
+        preparedStatement.setInt(1,sid);
         preparedStatement.setString(2,type);
 
         ResultSet resultSet = preparedStatement.executeQuery();
@@ -32,5 +34,27 @@ public class addPaymentService {
         System.out.println(preparedStatement);
         return Totalestimate;
 
+    }
+
+    public double totEstimates(int sid) throws SQLException, ClassNotFoundException {
+        con = DBConnectionUtil.getConnection();
+        double totalAmount =0.0;
+        addpayment addpay = null;
+        String query = "SELECT totalAmount FROM  rasa.workprogress WHERE sid = ?";
+        PreparedStatement preparedStatement = con.prepareStatement(query);
+        preparedStatement.setInt(1,sid);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()){
+            totalAmount = resultSet.getDouble("totalAmount");
+
+            addpay = new addpayment(totalAmount);
+        }
+
+
+
+        System.out.println(preparedStatement);
+        return totalAmount;
     }
 }
