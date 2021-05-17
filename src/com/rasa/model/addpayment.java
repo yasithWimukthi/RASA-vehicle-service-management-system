c29ad762340df742251946eec74c52d711fa11f5
tharukshi724 <tharukshiw@gmail.com>
2021-05-17 19:13:44 +0530

    modify paymentlist.jsp

--- src/com/rasa/model/addpayment.java
+++ src/com/rasa/model/addpayment.java
@@ -1,61 +1,24 @@
 package com.rasa.model;
 
 public class addpayment {
-    int serviceID;
-    private double removeRefting;
-    private double replacing;
-    private double painting;
-    private double totEstimateAmount;
 
-    public addpayment(int serviceID, double removeRefting, double replacing, double painting, double totEstimateAmount) {
-        this.serviceID = serviceID;
-        this.removeRefting = removeRefting;
-        this.replacing = replacing;
-        this.painting = painting;
-        this.totEstimateAmount = totEstimateAmount;
+    private double totalAmount;
+
+    public addpayment(double totalAmount) {
+
+        this.totalAmount = totalAmount;
     }
 
-    public addpayment(double removeRefting){
-        this.removeRefting = removeRefting;
+
+
+
+
+    public double getTotalAmount() {
+        return totalAmount;
     }
 
-    public int getServiceID() {
-        return serviceID;
-    }
-
-    public void setServiceID(int serviceID) {
-        this.serviceID = serviceID;
-    }
-
-    public double getRemoveRefting() {
-        return removeRefting;
-    }
-
-    public void setRemoveRefting(double removeRefting) {
-        this.removeRefting = removeRefting;
-    }
-
-    public double getReplacing() {
-        return replacing;
-    }
-
-    public void setReplacing(double replacing) {
-        this.replacing = replacing;
-    }
-
-    public double getPainting() {
-        return painting;
-    }
-
-    public void setPainting(double painting) {
-        this.painting = painting;
-    }
-
-    public double getTotEstimateAmount() {
-        return totEstimateAmount;
-    }
-
-    public void setTotEstimateAmount(double totEstimateAmount) {
-        this.totEstimateAmount = totEstimateAmount;
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount= totalAmount;
     }
 }
+
