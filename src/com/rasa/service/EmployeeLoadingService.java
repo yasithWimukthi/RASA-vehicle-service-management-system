a4e54ce7d4ba2eb3982dd3d2cc224dc52df0e753
iamKAviB <iamkavib@gmail.com>
2021-05-17 21:07:52 +0530

    fix errors

--- src/com/rasa/service/EmployeeLoadingService.java
+++ src/com/rasa/service/EmployeeLoadingService.java
@@ -13,13 +13,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EmployeeLoadingService  implements IEmpLoad{
+public class EmployeeLoadingService  {
 
     private static Connection con;
     private PreparedStatement preparedStatement;
     private PreparedStatement preparedStatement1;
 
-    @Override
+
     public List<Employee> loadToPaymentTable(){
 
         List<Employee> list =new ArrayList<>();
@@ -54,7 +54,7 @@ public class EmployeeLoadingService  implements IEmpLoad{
 
     }
 
-    @Override
+
     public List<Employee> loadToAttendanceTable(){
 
         List<Employee> list =new ArrayList<>();
@@ -73,10 +73,12 @@ public class EmployeeLoadingService  implements IEmpLoad{
                 String fname =(rs.getString(2));
                 String lname=(rs.getString(3));
 
+
                 Employee emp = new Employee();
                 emp.setEmployeeID(empID);
                 emp.setFname(fname);
                 emp.setLname(lname);
+
 
                 list.add(emp);
 
@@ -87,10 +89,117 @@ public class EmployeeLoadingService  implements IEmpLoad{
         return list;
 
     }
+    
 
-    public EmployeePayment toPaymentForm(int id){
+    public Employee getEmp(int id){
+        Employee emp  = new Employee(); 
+        try {
+            con = DBConnectionUtil.getConnection();
+            String sql="select * from employee where employeeID="+id;
+            preparedStatement = con.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
 
-        return null;
+           
+            
+            while(rs.next()){
+               int empID = rs.getInt(1);
+               String fname=rs.getString(2);
+               String lname = rs.getString(3);
+                Double bSal=(rs.getDouble(9));
+
+
+                
+                emp.setEmployeeID(empID);
+                emp.setFname(fname);
+                emp.setLname(lname);
+                emp.setBasicSalary(bSal);
+
+
+            }
+            
+           
+        }catch(Exception e){
+            
+            e.printStackTrace();
+        }
+        
+        return emp;
+    }
+    
+    
+
+    public double getAdvance(int id){
+
+        double adv = 0;
+        try {
+            con = DBConnectionUtil.getConnection();
+            String sql = "select * from advance where EXTRACT(month from adv_date)=EXTRACT(month from CURRENT_TIMESTAMP) and employeeID="+id;
+            preparedStatement = con.prepareStatement(sql);
+            
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while(rs.next()) {
+                adv = adv+rs.getDouble(4);
+
+            }
+           
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        
+        return adv;
+    }
+
+
+    public int getPresent(int id){
+
+        int present =0;
+        try{
+
+            con = DBConnectionUtil.getConnection();
+            String sql = "select * from attendance where employeeID="+id+" and status='present' and EXTRACT(month from att_date)=EXTRACT(month from CURRENT_TIMESTAMP)";
+            preparedStatement = con.prepareStatement(sql);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while(rs.next()){
+
+                present++;
+
+            }
+
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return present;
+    }
+
+    public int getHalfDAy(int id){
+        int half=0;
+        try{
+
+            con = DBConnectionUtil.getConnection();
+            String sql = "select * from attendance where employeeID="+id+" and status='halfday' and EXTRACT(month from att_date)=EXTRACT(month from CURRENT_TIMESTAMP)";
+            preparedStatement = con.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while(rs.next()){
+
+                half++;
+
+            }
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return half;
     }
 
 
